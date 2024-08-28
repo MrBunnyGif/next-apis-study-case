605fda7f3567a64c8cf76d8d115621733239c4e4
Pedro Botega <botegapedro@gmail.com>
2024-08-28 15:20:37 +0100

    Creates home initial texts

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">AI powered prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat
+      </p>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
