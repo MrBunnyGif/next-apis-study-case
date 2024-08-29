1a4eb5a90b1e8034f1b26732e947978322bab48e
Pedro Botega <botegapedro@gmail.com>
2024-08-29 15:03:23 +0100

    Creates provider initial structure

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+'use client'
 
-const Provider = () => {
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+
+const Provider = ({ children, session }) => {
   return (
-    <div>Provider</div>
-  )
-}
+    <>
+      <SessionProvider session={session}>{children}</SessionProvider>
+    </>
+  );
+};
 
-export default Provider
+export default Provider;
