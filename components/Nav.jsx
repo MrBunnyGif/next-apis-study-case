bcf339f3be7158999cab1f44b4fa292bd6703e5b
Pedro Botega <botegapedro@gmail.com>
2024-08-29 17:09:48 +0100

    Updates nav to use google image

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -18,6 +18,8 @@ const Nav = () => {
     })();
   }, []);
 
+  console.log(session);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -42,7 +44,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user?.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -71,7 +73,7 @@ const Nav = () => {
         {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user?.image}
               width={37}
               height={37}
               className="rounded-full"
